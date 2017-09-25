9d62fb61e4c0ec8d870a0f97a8fa65fd49009064
BurnellLiu <burnell_liu@outlook.com>
2017-09-25 17:49:41 +0800

    增加回归算法测试工程

---
+++ Test/TinyMLTest/Regression/Main.cpp
@@ -0,0 +1,45 @@
+﻿
+
+#include "../../../Src/LRegression.h"
+
+
+int main()
+{
+    // 定义训练样本
+    float trainX[4] =
+    {
+        2.0f,
+        4.0f,
+        6.0f,
+        8.0f
+    };
+    LRegressionMatrix xMatrix(4, 1, trainX);
+
+    // 定义训练样本输出
+    float trainY[4] =
+    {
+        1.0f,
+        2.0f,
+        3.0f,
+        4.0f
+    };
+    LRegressionMatrix yMatrix(4, 1, trainY);
+
+
+    // 定义线性回归对象
+    LLinearRegression linearReg(4, 1);
+
+    // 训练模型
+    for (unsigned int i = 0; i < 500; i++)
+    {
+        linearReg.TrainModel(xMatrix, yMatrix, 0.01f);
+    }
+
+    // 进行预测
+    LRegressionMatrix yVector;
+    linearReg.Predict(xMatrix, yVector);
+
+
+    return 0;
+}
+
