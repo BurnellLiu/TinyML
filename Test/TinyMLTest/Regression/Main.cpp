1c3642e1dfcfa056523cad5a0834f7f4683bf010
Burnellliu <burnell_liu@qq.com>
2018-01-31 22:03:06 +0800

    使用乳腺癌数据集测试逻辑回归

--- Test/TinyMLTest/Regression/Main.cpp
+++ Test/TinyMLTest/Regression/Main.cpp
@@ -6,6 +6,7 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <ctime>
 
 /// @brief 打印矩阵
 void MatrixPrint(IN const LDataMatrix& dataMatrix)
@@ -27,15 +28,63 @@ void TestLinearRegression()
 
 }
 
+/// @brief 测试逻辑回归
 void TestLogisticRegression()
 {
- 
+    // 加载乳腺癌数据集
+    LCSVParser csvParser(L"../../../DataSet/breast_cancer.csv");
+    csvParser.SetSkipHeader(true);
+    LDataMatrix dataMatrix;
+    csvParser.LoadAllData(dataMatrix);
+
+    // 数据进行缩放
+    LUIntMatrix colVec(1, 30);
+    for (unsigned int col = 0; col > colVec.ColumnLen; col++)
+    {
+        colVec[0][col] = col;
+    }
+    LMinMaxScaler scaler(0, 1.0);
+    scaler.FitTransform(colVec, dataMatrix);
+
+    // 打乱数据集
+    DoubleMatrixShuffle(0, dataMatrix);
+
+    // 将数据集拆分为训练集和测试集, 测试集占总集合的20%
+    unsigned int testSize = (unsigned int)(dataMatrix.RowLen * 0.2);
+    LDataMatrix trainData;
+    LDataMatrix testData;
+    dataMatrix.SubMatrix(0, testSize, 0, dataMatrix.ColumnLen, testData);
+    dataMatrix.SubMatrix(testSize, dataMatrix.RowLen - testSize, 0, dataMatrix.ColumnLen, trainData);
+
+    // 将训练集拆分为训练样本集合和标签集
+    LRegressionMatrix trainXMatrix;
+    LRegressionMatrix trainYVector;
+    trainData.SubMatrix(0, trainData.RowLen, 0, trainData.ColumnLen - 1, trainXMatrix);
+    trainData.SubMatrix(0, trainData.RowLen, trainData.ColumnLen - 1, 1, trainYVector);
+
+    // 将测试集拆分为测试样本集合和标签集
+    LRegressionMatrix testXMatrix;
+    LRegressionMatrix testYVector;
+    testData.SubMatrix(0, testData.RowLen, 0, testData.ColumnLen - 1, testXMatrix);
+    testData.SubMatrix(0, testData.RowLen, testData.ColumnLen - 1, 1, testYVector);
+
+    // 使用训练集训练模型
+    LLogisticRegression clf;
+    for (int i = 0; i < 1000; i++)
+    {
+        clf.TrainModel(trainXMatrix, trainYVector, 0.1);
+    }
+
+    // 使用测试集计算模型得分
+    double score = clf.Score(testXMatrix, testYVector);
+    printf("Logistic Regression Model Score: %.2f\n", score);
+
 }
 
 /// @brief 测试SoftMax回归
 void TestSoftmaxRegression()
 {
-    // 加载数据集
+    // 加载鸢尾花数据集
     LCSVParser csvParser(L"../../../DataSet/iris.csv");
     csvParser.SetSkipHeader(true);
     LDataMatrix dataMatrix;
@@ -93,7 +142,7 @@ void TestSoftmaxRegression()
         clf.TrainModel(trainXMatrix, trainYMatrix, 0.1);
     }
 
-    // 计算模型得分
+    // 使用测试集计算模型得分
     double score = clf.Score(testXMatrix, testYMatrix);
     printf("Softmax Regression Model Score: %.2f\n", score);
 
@@ -102,8 +151,8 @@ void TestSoftmaxRegression()
 int main()
 {
     // TestLinearRegression();
-    // TestLogisticRegression();
-    TestSoftmaxRegression();
+    TestLogisticRegression();
+    // TestSoftmaxRegression();
 
     system("pause");
 
