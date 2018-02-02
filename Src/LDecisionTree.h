bc0f5de83f93500e39b920a4fbd172027edc94d1
BurnellLiu <burnell_liu@outlook.com>
2018-02-02 17:17:32 +0800

    开始实现回归树

--- Src/LDecisionTree.h
+++ Src/LDecisionTree.h
@@ -25,7 +25,7 @@ typedef LMatrix<double> LDTMatrix;     ///< 决策树矩阵
 
 class CDecisionTreeClassifier;
 
-/// @brief 决策树分类器
+/// @brief 分类树
 class LDecisionTreeClassifier
 {
 public:
@@ -38,7 +38,7 @@ public:
     /// @brief 训练模型
     /// 每使用一次该方法, 则生成一个新的模型
     /// @param[in] xMatrix 样本矩阵, 每一行代表一个样本, 每一列代表样本的一个特征
-    /// @param[in] nVector 样本特征分布向量(行向量), 每一列代表一个特征的分布, 值只能为FEATURE_DISCRETE和FEATURE_CONTINUUM
+    /// @param[in] nVector 样本特征分布向量(行向量), 每一列代表一个特征的分布, 值只能为DT_FEATURE_DISCRETE和DT_FEATURE_CONTINUUM
     /// @param[in] yVector 样本标签向量(列向量), 每一行代表一个样本, 标签值应为离散值, 不同的值代表不同的类别
     /// @return 成功返回true, 失败返回false(参数错误的情况下会返回失败)
     bool TrainModel(IN const LDTMatrix& xMatrix, IN const LDTMatrix& nVector, IN const LDTMatrix& yVector);
@@ -65,7 +65,53 @@ public:
     void PrintTree();
 
 private:
-    CDecisionTreeClassifier* m_pClassifier; ///< 决策树分类器实现对象
+    CDecisionTreeClassifier* m_pClassifier; ///< 分类树实现对象
+};
+
+
+class CDecisionTreeRegression;
+
+/// @brief 回归树
+class LDecisionTreeRegression
+{
+public:
+    /// @brief 构造函数
+    LDecisionTreeRegression();
+
+    /// @brief 析构造函数
+    ~LDecisionTreeRegression();
+
+    /// @brief 训练模型
+    /// 每使用一次该方法, 则生成一个新的模型
+    /// @param[in] xMatrix 样本矩阵, 每一行代表一个样本, 每一列代表样本的一个特征
+    /// @param[in] nVector 样本特征分布向量(行向量), 每一列代表一个特征的分布, 值只能为DT_FEATURE_DISCRETE和DT_FEATURE_CONTINUUM
+    /// @param[in] yVector 样本向量(列向量), 每一行代表一个样本
+    /// @return 成功返回true, 失败返回false(参数错误的情况下会返回失败)
+    bool TrainModel(IN const LDTMatrix& xMatrix, IN const LDTMatrix& nVector, IN const LDTMatrix& yVector);
+
+    /// @brief 进行剪枝操作(即合并叶子节点)
+    /// 剪枝可以防止模型过拟合
+    /// 如果两个叶子节点合并后增加的熵小于指定的最小值, 则进行合并操作
+    /// @param[in] minGain 最小增加值
+    // void Prune(IN double minGain);
+
+    /// @brief 使用训练好的模型预测数据
+    /// @param[in] xMatrix 需要预测的样本矩阵
+    /// @param[out] yVector 存储预测的标签向量(列向量)
+    /// @return 成功返回true, 失败返回false(模型未训练或参数错误的情况下会返回失败)
+    bool Predict(IN const LDTMatrix& xMatrix, OUT LDTMatrix& yVector) const;
+
+    /// @brief 计算模型得分
+    /// @param[in] xMatrix 样本矩阵
+    /// @param[in] yVector 标签向量(列向量)
+    /// @return 得分 值为0.0~1.0, 模型未训练或者参数有误返回-1.0
+    double Score(IN const LDTMatrix& xMatrix, IN const LDTMatrix& yVector) const;
+
+    /// @brief 打印树, 用于调试
+    void PrintTree();
+
+private:
+    CDecisionTreeRegression* m_pClassifier; ///< 决策树分类器实现对象
 };
 
 
