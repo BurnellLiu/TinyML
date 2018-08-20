a247714c1bc4e6e988bf6972aebb00d70467d36a
BurnellLiu <burnell_liu@outlook.com>
2018-08-20 17:39:08 +0800

    update

--- Src/LReinforcementLearning.h
+++ Src/LReinforcementLearning.h
@@ -38,10 +38,9 @@ using std::map;
 
 typedef set<unsigned int>               LStateSet;          // 状态集合
 typedef set<unsigned int>               LActionSet;         // 动作集合
-typedef map<unsigned int, double>       LStateProbSet;      // 状态概率集合, 键为状态, 值为概率
-typedef map<unsigned int, double>       LStateValueSet;     // 状态值集合, 键位状态, 值为状态的回报 
-typedef map<unsigned int, unsigned int> LPolicyStable;      // 策略表, 
-
+typedef map<unsigned int, double>       LStateProbTable;    // 状态概率表, 键为状态, 值为概率
+typedef map<unsigned int, double>       LStateValueTable;   // 状态回报表, 键为状态, 值为状态的回报 
+typedef map<unsigned int, unsigned int> LPolicyTable;       // 策略表, 键为状态, 值为动作
 
 
 /// @brief 立即回报函数
@@ -51,33 +50,73 @@ typedef map<unsigned int, unsigned int> LPolicyStable;      // 策略表,
 /// @return 成功返回true, 失败返回false
 typedef bool (*RewardFunc)(IN int s, IN int a, OUT double* pReward);
 
-/// @brief 状态转移函数
+/// @brief 状态转移概率函数
 /// @param[in] s 状态值
 /// @param[in] a 动作值
 /// @param[out] pStateProbVec 存储s状态下进行动作a后转移到各个状态的概率
 /// @return 成功返回true, 失败返回false
-typedef bool(*StateProbFunc)(IN int s, IN int a, OUT LStateProbSet* pStateProbVec);
+typedef bool(*StateProbFunc)(IN int s, IN int a, OUT LStateProbTable* pStateProbTable);
+
+class CMDPDynamicProgram;
 
 /// @brief 策略迭代
 class LPolicyIteration
 {
 public:
     /// @brief 构造函数
-    LPolicyIteration(IN const LStateSet& stateVec, IN const LActionSet& actionVec);
+    /// @param[in] stateSet 状态集合, 不能为空集合
+    /// @param[in] actionSet 动作集合, 不能为空集合
+    LPolicyIteration(IN const LStateSet& stateSet, IN const LActionSet& actionSet);
 
     /// @brief 析构函数
     ~LPolicyIteration();
 
     /// @brief 训练模型
     /// @param[in] r 立即回报函数
-    /// @param[in] p 状态转移函数
-    /// @param[in] theta 阈值(终止条件, 状态值变化的最小量)
+    /// @param[in] p 状态转移概率函数
+    /// @param[in] theta 阈值(终止条件, 状态值变化的最小量, 正值)
+    /// @param[in] gamma 折合因子(未来回报的重要程度), [0-1]
     /// @return 成功返回true, 失败返回false(参数错误的情况下会返回失败)
-    bool TrainModel(IN RewardFunc r, IN StateProbFunc p, IN double theta);
+    bool TrainModel(IN RewardFunc r, IN StateProbFunc p, IN double theta, IN double gamma);
 
-    bool GetModel();
+    /// @brief 获取状态值表
+    const LStateValueTable& GetStateValueTable();
+
+    /// @brief 获取策略值表
+    const LPolicyTable& GetPolicyTable();
 
 private:
+    CMDPDynamicProgram* m_pMDPDynamicProgram;       // 动态规划算法对象
+};
+
+/// @brief 值迭代
+class LValueIteration
+{
+public:
+    /// @brief 构造函数
+    /// @param[in] stateSet 状态集合, 不能为空集合
+    /// @param[in] actionSet 动作集合, 不能为空集合
+    LValueIteration(IN const LStateSet& stateSet, IN const LActionSet& actionSet);
+
+    /// @brief 析构函数
+    ~LValueIteration();
+
+    /// @brief 训练模型
+    /// @param[in] r 立即回报函数
+    /// @param[in] p 状态转移概率函数
+    /// @param[in] theta 阈值(终止条件, 状态值变化的最小量, 正值)
+    /// @param[in] gamma 折合因子(未来回报的重要程度), [0-1]
+    /// @return 成功返回true, 失败返回false(参数错误的情况下会返回失败)
+    bool TrainModel(IN RewardFunc r, IN StateProbFunc p, IN double theta, IN double gamma);
+
+    /// @brief 获取状态值表
+    const LStateValueTable& GetStateValueTable();
+
+    /// @brief 获取策略值表
+    const LPolicyTable& GetPolicyTable();
+
+private:
+    CMDPDynamicProgram* m_pMDPDynamicProgram;       // 动态规划算法对象
 };
 
 #endif
