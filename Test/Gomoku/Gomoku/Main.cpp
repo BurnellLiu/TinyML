6d19cf300dbd698af719ea5665f3e0103134b469
BurnellLiu <burnell_liu@outlook.com>
2018-09-14 17:43:40 +0800

    update

--- Test/Gomoku/Gomoku/Main.cpp
+++ Test/Gomoku/Gomoku/Main.cpp
@@ -487,6 +487,7 @@ int main()
     /*
     https://wanjun0511.github.io/2017/11/05/DQN/
     https://zhuanlan.zhihu.com/p/32089487
+	https://charlesliuyx.github.io/2017/10/18/%E6%B7%B1%E5%85%A5%E6%B5%85%E5%87%BA%E7%9C%8B%E6%87%82AlphaGo%E5%85%83/
     */
     Train();
     //Test(".\\10000-Train.ai");
