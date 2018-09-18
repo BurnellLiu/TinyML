e992d55b7fb00523d842a458dfba0658547c5772
BurnellLiu <burnell_liu@outlook.com>
2018-09-18 18:03:02 +0800

    update

--- Test/TaiChiGomoku/gomoku_game.py
+++ Test/TaiChiGomoku/gomoku_game.py
@@ -29,88 +29,89 @@ class GomokuBoard(object):
         :param kwargs: 关键字参数
         """
         # 棋盘宽度
-        self.__width = int(kwargs.get('width', 8))
+        self.width = int(kwargs.get('width', 4))
         # 棋盘高度
-        self.__height = int(kwargs.get('height', 8))
+        self.height = int(kwargs.get('height', 4))
         # 胜利连子数
-        self.__n_in_row = int(kwargs.get('n_in_row', 4))
+        self.n_in_row = int(kwargs.get('n_in_row', 3))
         # 最近动作
-        self.__last_action = -1
+        self.last_action = -1
         # 黑子先手
-        self.__current_player = GomokuPlayer.Black
+        self.current_player = GomokuPlayer.Black
         # 可用的动作
-        self.__avl_actions = list(range(self.__width * self.__height))
+        self.avl_actions = list(range(self.width * self.height))
         # 当前棋盘状态
-        self.__state = [[GomokuPlayer.Nobody for col in range(self.__width)] for row in range(self.__height)]
-        if self.__width < self.__n_in_row or self.__height < self.__n_in_row:
+        self.state = [[GomokuPlayer.Nobody for col in range(self.width)] for row in range(self.height)]
+        if self.width < self.n_in_row or self.height < self.n_in_row:
             raise Exception('GomokuBoard width and height can not be '
-                            'less than {}'.format(self.__n_in_row))
+                            'less than {}'.format(self.n_in_row))
 
     def move(self, action):
         """
         下子
         :param action: 动作
-        :return: (游戏是否结束, 赢家)
         """
-        row, col = self.__action_to_loc(action)
-        self.__state[row][col] = self.__current_player
-        self.__avl_actions.remove(action)
-        self.__last_action = action
+        if action not in self.avl_actions:
+            raise Exception('GomokuBoard move action error ')
 
-        # 检查游戏是否结束, 以及赢家
-        game_end, winder = self.__check_winner()
+        row, col = self.action_to_loc(action)
+        self.state[row][col] = self.current_player
+        self.avl_actions.remove(action)
+        self.last_action = action
 
         # 改变当前玩家
-        if self.__current_player == GomokuPlayer.Black:
-            self.__current_player = GomokuPlayer.White
+        if self.current_player == GomokuPlayer.Black:
+            self.current_player = GomokuPlayer.White
         else:
-            self.__current_player = GomokuPlayer.Black
+            self.current_player = GomokuPlayer.Black
 
-        return game_end, winder
-
-    def __check_winner(self):
+    def check_winner(self):
         """
         检查胜利者
         :return: (游戏是否结束, 赢家)
         """
-        act_row, act_col = self.__action_to_loc(self.__last_action)
+        if self.last_action == -1:
+            return False, GomokuPlayer.Nobody
+
+        act_row, act_col = self.action_to_loc(self.last_action)
+        last_player = self.state[act_row][act_col]
 
         # 检查横向是否达成连子
         spot_count = 1
         for col in range(act_col-1, -1, -1):
-            if self.__state[act_row][col] == self.__current_player:
+            if self.state[act_row][col] == last_player:
                 spot_count += 1
             else:
                 break
-        for col in range(act_col + 1, self.__width):
-            if self.__state[act_row][col] == self.__current_player:
+        for col in range(act_col + 1, self.width):
+            if self.state[act_row][col] == last_player:
                 spot_count += 1
             else:
                 break
-        if spot_count >= self.__n_in_row:
-            return True, self.__current_player
+        if spot_count >= self.n_in_row:
+            return True, last_player
 
         # 检查纵向是否达成连子
         spot_count = 1
         for row in range(act_row-1, -1, -1):
-            if self.__state[row][act_col] == self.__current_player:
+            if self.state[row][act_col] == last_player:
                 spot_count += 1
             else:
                 break
-        for row in range(act_row + 1, self.__height):
-            if self.__state[row][act_col] == self.__current_player:
+        for row in range(act_row + 1, self.height):
+            if self.state[row][act_col] == last_player:
                 spot_count += 1
             else:
                 break
-        if spot_count >= self.__n_in_row:
-            return True, self.__current_player
+        if spot_count >= self.n_in_row:
+            return True, last_player
 
         # 检查斜线是否连成5子
         spot_count = 1
         row = act_row-1
         col = act_col-1
         while row >= 0 and col >= 0:
-            if self.__state[row][col] == self.__current_player:
+            if self.state[row][col] == last_player:
                 spot_count += 1
             else:
                 break
@@ -118,22 +119,22 @@ class GomokuBoard(object):
             col -= 1
         row = act_row+1
         col = act_col+1
-        while row < self.__height and col < self.__width:
-            if self.__state[row][col] == self.__current_player:
+        while row < self.height and col < self.width:
+            if self.state[row][col] == last_player:
                 spot_count += 1
             else:
                 break
             row += 1
             col += 1
-        if spot_count >= self.__n_in_row:
-            return True, self.__current_player
+        if spot_count >= self.n_in_row:
+            return True, last_player
 
         # 检查反斜线是否连成5子
         spot_count = 1
         row = act_row+1
         col = act_col-1
-        while row < self.__height and col >= 0:
-            if self.__state[row][col] == self.__current_player:
+        while row < self.height and col >= 0:
+            if self.state[row][col] == last_player:
                 spot_count += 1
             else:
                 break
@@ -141,22 +142,22 @@ class GomokuBoard(object):
             col -= 1
         row = act_row-1
         col = act_col+1
-        while row >= 0 and col < self.__width:
-            if self.__state[row][col] == self.__current_player:
+        while row >= 0 and col < self.width:
+            if self.state[row][col] == last_player:
                 spot_count += 1
             else:
                 break
             row -= 1
             col += 1
-        if spot_count >= self.__n_in_row:
-            return True, self.__current_player
+        if spot_count >= self.n_in_row:
+            return True, last_player
 
-        if len(self.__avl_actions) == 0:
+        if len(self.avl_actions) == 0:
             return True, GomokuPlayer.Nobody
 
         return False, GomokuPlayer.Nobody
 
-    def __action_to_loc(self, action):
+    def action_to_loc(self, action):
         """
         动作转换为位置
         3*3 棋盘如下:
@@ -167,14 +168,14 @@ class GomokuBoard(object):
         :param action: 动作值
         :return: 位置元组(row, col)
         """
-        if action not in range(self.__width * self.__height):
+        if action not in range(self.width * self.height):
             raise Exception('Action error: {}'.format(action))
 
-        row = action // self.__width
-        col = action % self.__width
+        row = action // self.width
+        col = action % self.width
         return row, col
 
-    def __loc_to_action(self, loc):
+    def loc_to_action(self, loc):
         """
         位置转换为动作
         :param loc: 位置
@@ -184,12 +185,30 @@ class GomokuBoard(object):
             raise Exception('Location error: {}'.format(loc))
         row = loc[0]
         col = loc[1]
-        action = row * self.__width + col
+        action = row * self.width + col
 
-        if action not in range(self.__width * self.__height):
+        if action not in range(self.width * self.height):
             raise Exception('Location error: {}'.format(loc))
         return action
 
 
 if __name__ == '__main__':
     board = GomokuBoard()
+    board.move(board.loc_to_action((0, 0)))
+    print(board.check_winner())
+    board.move(board.loc_to_action((7, 0)))
+    print(board.check_winner())
+    board.move(board.loc_to_action((1, 1)))
+    print(board.check_winner())
+    board.move(board.loc_to_action((6, 1)))
+    print(board.check_winner())
+    board.move(board.loc_to_action((2, 2)))
+    print(board.check_winner())
+    board.move(board.loc_to_action((5, 2)))
+    print(board.check_winner())
+    board.move(board.loc_to_action((3, 3)))
+    print(board.check_winner())
+    board.move(board.loc_to_action((4, 3)))
+    print(board.check_winner())
+
+
--- Test/TaiChiGomoku/mcts_pure.py
+++ Test/TaiChiGomoku/mcts_pure.py
@@ -8,7 +8,37 @@
 @email: coderjie@outlook.com
 """
 
+import copy
 import numpy as np
+
+from gomoku_game import GomokuPlayer
+from gomoku_game import GomokuBoard
+from operator import itemgetter
+
+
+def rollout_policy_value_(state):
+
+    avg_probs = np.ones(len(state.avl_actions))/len(state.avl_actions)
+    action_probs = list(zip(state.avl_actions, avg_probs))
+    state_value = 0.0
+
+    current_player = state.current_player
+    for i in range(state.width * state.height):
+        rand_probs = np.random.rand(len(state.avl_actions))
+        rand_action_probs = zip(state.avl_actions, rand_probs)
+        max_action = max(rand_action_probs, key=itemgetter(1))[0]
+        state.move(max_action)
+        end, winner = state.check_winner()
+        if end:
+            if winner == current_player:
+                state_value = 1.0
+            elif winner == GomokuPlayer.Nobody:
+                state_value = 0.0
+            else:
+                state_value = -1.0
+            break
+
+    return action_probs, state_value
 
 
 class MCTSNode(object):
@@ -24,13 +54,13 @@ class MCTSNode(object):
         :param c_uct: 上限置信区间参数
         """
         self.__parent = parent          # 父节点
-        self.__children = {}            # 子节点表
+        self.children = {}              # 子节点表, 键: 动作 值: 节点
 
         self.__c_uct = c_uct            # 上限置信区间参数
 
         self.__action_prob = prob       # 动作先验概率
-        self.__action_q = 0.0           # 动作值
-        self.__action_n = 0             # 动作访问次数
+        self.__action_q = 0.0           # 动作值, 代表当前状态下, 接下来下子的玩家的局面状况
+        self.action_n = 0               # 动作访问次数
 
     def expand(self, actions_prob):
         """
@@ -38,15 +68,15 @@ class MCTSNode(object):
         :param actions_prob: 动作先验概率列表
         """
         for action, prob in actions_prob:
-            if action not in self.__children:
-                self.__children[action] = MCTSNode(self, prob)
+            if action not in self.children:
+                self.children[action] = MCTSNode(self, prob)
 
     def select(self):
         """
         选择一个动作
         :return: 返回(动作, 节点)元组
         """
-        return max(self.__children.items(),
+        return max(self.children.items(),
                    key=lambda action_node: action_node[1].__get_value())
 
     def backup(self, leaf_value):
@@ -64,7 +94,7 @@ class MCTSNode(object):
         是否是叶子节点
         :return: 叶子节点返回True, 否正返回False
         """
-        return self.__children == {}
+        return self.children == {}
 
     def is_root(self):
         """
@@ -78,30 +108,46 @@ class MCTSNode(object):
         更新节点, 更新动作值为模拟过程的平均值
         :param leaf_value: 叶子节点(终局)评估值
         """
-        sum_value = self.__action_q * self.__action_n + leaf_value
+        sum_value = self.__action_q * self.action_n + leaf_value
         # 访问次数+1
-        self.__action_n += 1
+        self.action_n += 1
         # 更新动作值
-        self.__action_q = sum_value/self.__action_n
+        self.__action_q = sum_value/self.action_n
 
     def __get_value(self):
         """
         获取调整后的动作值
         :return:
         """
-        u = self.__c_uct * self.__action_prob / (1 + self.__action_n)
+        u = self.__c_uct * self.__action_prob / (1 + self.action_n)
         return self.__action_q + u
 
     def __str__(self):
         return "MCTSNode: Prob: {}, Q: {}, N: {}".format(
-            self.__action_prob, self.__action_q, self.__action_n)
+            self.__action_prob, self.__action_q, self.action_n)
 
 
 class MCTSPure:
-    def __init__(self):
+    def __init__(self, policy_value_fn=rollout_policy_value_, play_out_n=10000):
+        # 根节点
+        self.__root = MCTSNode(None, 1.0)
+        # 策略值函数
+        self.__policy_value = policy_value_fn
+        self.__play_out_n = play_out_n
+
+    def get_action_probs(self, state):
+        for i in range(self.__play_out_n):
+            state_copy = copy.deepcopy(state)
+            self.__play_out(state_copy)
+        act_visits = [(act, node.action_n)for act, node in self.__root.children.items()]
+        acts, visits = zip(*act_visits)
+        probs = visits/np.sum(visits)
+
         self.__root = MCTSNode(None, 1.0)
 
-    def play_out(self, state):
+        return acts, probs
+
+    def __play_out(self, state):
         current_node = self.__root
         while True:
             if current_node.is_leaf():
@@ -109,13 +155,32 @@ class MCTSPure:
             action, current_node = current_node.select()
             state.move(action)
 
-        end, winner = state.game_end()
+        end, winner = state.check_winner()
+
+        if not end:
+            probs, state_value = self.__policy_value(state)
+            current_node.expand(probs)
+            current_node.backup(state_value)
+        else:
+            # 游戏和棋结束
+            if winner == GomokuPlayer.Nobody:
+                current_node.backup(0)
+            else:
+                # 游戏不是和棋结束, 则代表当前状态下, 接下来下子的玩家输棋, 所以设置评估值为-1
+                current_node.backup(-1)
 
 
 if __name__ == '__main__':
+    board = GomokuBoard()
+    mcts = MCTSPure()
 
-    node = MCTSNode(None, 1.0)
-    node.expand([(0, 0.1), (1, 0.2), (2, 0.5), (3, 0.2)])
+    while True:
+        end, winner = board.check_winner()
+        if end:
+            break
 
-    print(node)
-    print(node.select()[1])
+        acts, probs = mcts.get_action_probs(board)
+        move = np.random.choice(acts, p=probs)
+        board.move(move)
+        print(board.action_to_loc(move))
+
